07fb0303c271e40dce38f267f05a7985d54f37c7
sharad <sharad.rajore@gmail.com>
2022-05-09 15:10:12 +0530

    Removed Hello

--- stock-application/src/main/java/com/zensar/stockapplication/StockApplication.java
+++ stock-application/src/main/java/com/zensar/stockapplication/StockApplication.java
@@ -14,7 +14,7 @@ public class StockApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		
 		SpringApplication.run(StockApplication.class, args);  
-		System.out.println("Hello");
+		
 
 	}
 	
